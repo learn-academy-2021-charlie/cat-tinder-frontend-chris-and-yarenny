cd99f1706078b70bdc7e1709b0bb965ab438154f
christophermackey <c.mackey@me.com>
2021-08-23 14:41:29 -0700

    creates component files

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+    render() {
+        return (
+            <>
+                <h3>This is a footer</h3>
+            </>
+        )
+    }
+}
+
+export default Footer
